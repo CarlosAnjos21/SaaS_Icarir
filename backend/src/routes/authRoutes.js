644594fc50bba69ad8694b6dcf2c73bf9b958599
Davi Henrique Lima Carvalho <davihenriquelcdev@gmail.com>
2025-11-11 18:09:48 -0300

    adicionado swagger Admin-card e Admin-Missoes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
+// Rotas Públicas (não precisam de authMiddleware)
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/refresh', authController.refreshToken); // Endpoint de renovação
+
+// Rotas Protegidas (precisam de authMiddleware)
+router.post('/logout', authMiddleware, authController.logout);
+router.get('/me', authMiddleware, authController.getMe); // Endpoint do perfil logado
+
+module.exports = router;
+
+
 /**
  * @swagger
  * tags:
@@ -122,14 +134,3 @@ const authMiddleware = require('../middlewares/authMiddleware');
  *         description: Token inválido ou expirado
  */
 
-
-// Rotas Públicas (não precisam de authMiddleware)
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/refresh', authController.refreshToken); // Endpoint de renovação
-
-// Rotas Protegidas (precisam de authMiddleware)
-router.post('/logout', authMiddleware, authController.logout);
-router.get('/me', authMiddleware, authController.getMe); // Endpoint do perfil logado
-
-module.exports = router;
