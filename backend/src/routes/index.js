fa26d2b3294de01f690798f6e47bfa97afe81a88
Carlos Anjos <carlosotacilio65@gmail.com>
2026-03-25 14:50:20 -0300

    Refatorar categorias de tarefas: renomear controller e rotas para taskCategories

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { authenticate } = require("../middlewares/authMiddleware");
 
-const authRoutes = require('./authRoutes');
-const userRoutes = require('./userRoutes');
-const missionRoutes = require('./missionRoutes');
-const adminRoutes = require('./adminRoutes');
-const quizRoutes = require('./quizRoutes');
-const validatorRoutes = require('./validatorRoutes');
-const rankingRoutes = require('./rankingRoutes');
-const taskRoutes = require('./taskRoutes');
-const categoriasTarefasRoutes = require('./categoriasTarefasRoutes');
+const authRoutes = require("./authRoutes");
+router.use("/auth", authRoutes);
 
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-router.use('/missions', missionRoutes);
-router.use('/admin', adminRoutes);
-router.use('/quizzes', quizRoutes);
-router.use('/validations', validatorRoutes);
-router.use('/ranking', rankingRoutes);
-router.use('/tasks', taskRoutes);
-router.use('/categorias-tarefas', categoriasTarefasRoutes);
+const userRoutes = require("./userRoutes");
+const missionRoutes = require("./missionRoutes");
+const quizRoutes = require("./quizRoutes");
+const rankingRoutes = require("./rankingRoutes");
+const taskRoutes = require("./taskRoutes");
+const validatorRoutes = require("./validatorRoutes");
+const taskCategoriesRoutes = require("./taskCategoriesRoutes");
+
+router.use("/users",           authenticate, userRoutes);
+router.use("/missions",        authenticate, missionRoutes);
+router.use("/quizzes",         authenticate, quizRoutes);
+router.use("/ranking",         authenticate, rankingRoutes);
+router.use("/tasks",           authenticate, taskRoutes);
+router.use("/validations",     authenticate, validatorRoutes);
+router.use("/task-categories", authenticate, taskCategoriesRoutes); 
+
+const adminRoutes = require("./adminRoutes");
+router.use("/admin", adminRoutes);
 
 module.exports = router;
